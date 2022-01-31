8c8f743ebd471fd95f2c511081bfcb0176a1ab37
SMITamim <98738396+SMITamim@users.noreply.github.com>
2022-01-31 17:16:40 +0600

    Add files via upload

---
+++ Task01.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <b>
+        Registration Form
+</b>
+</head>
+<hr>
+<form>
+    <label for="First Name">
+        First name:         
+
+    </label>
+    
+    <input type="text" id="first name" name="first name">
+    <br>
+    <br>
+
+    <label for="last name">
+        Last name:
+    </label>
+    
+  <input type="text" id="last name" name="last name">
+  <br>
+  <br>
+
+  <label for="Age">
+      Age:
+      
+  </label>
+  </label>
+  
+  <input type="number" id="Age" name="Age">
+  <br>
+  <br>
+
+  <label for="Des">
+      Designation:
+  </label>
+
+  <input type="radio" id="Des" name="Des" value="Juniour Programmer">
+  <label for="Juniour Programmer">
+    Juniour Programmer
+  </label>
+
+  <input type="radio" id="Des" name="Des" value="S.P">
+  <label for="S.P">
+      Seniour Programmer 
+  </label>
+
+  <input type="radio" id="Des" name="Des" value="P.L">
+  <label for="P.L">
+      Project Lead
+  </label>
+
+  <br>
+  <br>
+
+  <label for="P. Lan">
+      Preferred Language:
+  </label>
+
+  <input type="checkbox" id="P. Lan" name="P. Lan" value="JAVA">
+  <label for="JAVA">
+      JAVA
+  </label>
+
+  <input type="checkbox" id="P. Lan" name="P. Lan" value="PHP">
+  <label for="PHP">
+      PHP
+  </label>
+
+  <input type="checkbox" id="P. Lan" name="P. Lan" value="C++">
+  <label for="C++">
+      C++
+  </label>
+  <br>
+  <br>
+
+  <label for="Email">
+      Email:
+  </label>
+  <input type="text" id="first name" name="Email">
+  <br>
+  <br>
+
+  <label for="Password">
+      Password
+  </label>
+  <input type="text" id="first name" name="Password">
+  <br>
+  <br>
+
+  <label for="file">
+      Please choose a file:
+  </label>
+
+  <input type="file" id="first name" name="first name">
+  <br>
+  <br>
+
+  <input type="submit" value="Submit">
+  <input type="reset" value="Reset">
+
+
+  
+
+
+  </form>
+
